093d501c5b045bd33933fe7ecd3bc22279237097
Bluscream <admin@timo.de.vc>
2020-08-28 15:14:13 +0200

    Add project files.

---
+++ .vshistory/Program.cs/2020-08-28_14_14_48_711.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unity_VR_Patcher
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+    }
+}
---
+++ .vshistory/Program.cs/2020-08-28_15_07_27_058.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace UnityVRPatcher
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main(List<string> args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var patcher = new UnityVRPatcher();
+        }
+
+        [DllImport("User32.dll")]
+        public static extern int SetForegroundWindow(IntPtr point);
+    }
+
+    internal class UnityVRPatcher
+    {
+        internal DirectoryInfo gameDir;
+
+        internal UnityVRPatcher()
+        {
+            // sdakfjniksodf
+        }
+    }
+}
