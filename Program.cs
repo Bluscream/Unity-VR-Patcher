e428773b89ef559a4af3356362d87dd1e4183f73
Bluscream <admin@timo.de.vc>
2020-08-28 15:14:13 +0200

    remove vshistory+Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using GModMountManager;
+
+namespace UnityVRPatcher
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main(List<string> args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var patcher = new UnityVRPatcher();
+        }
+
+        [DllImport("User32.dll")]
+        public static extern int SetForegroundWindow(IntPtr point);
+    }
+
+    internal class UnityVRPatcher
+    {
+        internal DirectoryInfo gameDir;
+
+        internal UnityVRPatcher()
+        {
+            var file = Utils.pickFile("Select ", null, "globalgamemanagers|globalgamemanagers");
+            if (!file.Exists) return;
+            gameDir = file.Directory.Parent;
+        }
+    }
+}
